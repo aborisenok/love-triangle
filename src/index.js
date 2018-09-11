b2d1f751f569f355131d2b686f5d00a4c5d77a94
aborisenok <lesha_borisenok@mail.ru>
2018-09-11 20:11:28 +0300

    well done

--- src/index.js
+++ src/index.js
@@ -3,5 +3,23 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+   var arr = preferences;
+  var triangles = 0;
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] !== -1){
+      var trio = [];
+      trio.push(arr[i]);
+      trio.push(arr[trio[0]-1]);
+      trio.push(arr[trio[1]-1]);
+      if((trio[2]-1) == i && trio[0] != trio[1] && trio[1] != trio[2]){
+        triangles++;
+        arr[i] = -1;
+        arr[trio[0]-1] = -1;
+        arr[trio[1]-1] = -1;
+      }
+      
+    }
+  }
+ 
+  return triangles;
 };
